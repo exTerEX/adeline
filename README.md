349a4563923f7588c4729a35c1a132c15ff8a1c8
exTerEX <30706568+exTerEX@users.noreply.github.com>
2021-08-02 00:46:33 +0200

    Updated readme and removed unusable example

---
+++ README.md
@@ -0,0 +1,18 @@
+# Adeline
+
+A simple implementation of Adaptive Linear Element (AdeLinE), an early single-layer artificial neural network.
+
+The output of the network is
+
+$$ y = \sum\limits_{j=1}^n x_jw_j+\theta$$
+
+Where,
+- $x$ is the input vector
+- $w$ is the weight vector
+- $n$ is the number of inputs
+- $\theta$ some constant
+- $y$ is the output of the model
+
+## License
+
+This repository is distrbuted under `Apache-2.0`. For more information see [LICENSE](LICENSE).
--- main.c
+++ main.c
@@ -1,28 +1,4 @@
 #include "adeline.h"
 #include <stddef.h>
 
-int main(int argc, char **argv) {
-    const int N = 10;
-
-    adeline_t ada = create_adaline(3, 0.1);
-
-    const double samples[10][3] = {{8, 1, 2},   {2, -8, -3}, {-1, 3, 7}, {3, -1, -6}, {2, 1, 6},
-                                   {3, -2, -8}, {-3, -3, 7}, {-3, 6, 9}, {-4, 2, 8},  {-5, -7, 9}};
-
-    double **X = (double **)malloc(N * sizeof(double *));
-    const size_t Y[10] = {1, -1, 1, -1, -1, -1, 1, 1, 1, -1};
-
-    for (size_t index = 0; index < N; index++) {
-        X[index] = (double *)samples[index];
-    }
-
-    fit_adaline(&ada, X, Y, N);
-
-    double test[] = {3, -3};
-    size_t pred = predict_adaline(&ada, test, NULL);
-
-    printf("Predict for x=(3,-3): % d", pred);
-
-    free(X);
-    delete_adaline(&ada);
-}
+int main(int argc, char **argv) { return 0; }
